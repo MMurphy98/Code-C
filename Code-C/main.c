d43c5769898b1ca70f08fb81d7932b54ab316107
MMurphy98 <mjg654654@outlook.com>
2022-03-30 10:03:36 +0800

    Initial Commit

---
+++ Code-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Code-C
+//
+//  Created by Yizli on 2022/3/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
