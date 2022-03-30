252e5cdc115266d1c02c02c2b097c3e6fe4e058c
MMurphy98 <mjg654654@outlook.com>
2022-03-30 10:27:43 +0800

    add for cycle

--- Code-C/main.c
+++ Code-C/main.c
@@ -10,5 +10,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    for (int i=0; i<10; i++) {
+        printf("Index = %d;\n",i);
+    }
     return 0;
 }
